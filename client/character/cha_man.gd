da854e56d2ddeaadb9615351587deedbc40a709f
Albert Cameron <qq79402005@gmail.com>
2017-07-03 19:31:25 +0800

    53 确定动画使用方式，并制作技能，跑，走，idle

--- client/character/cha_man.gd
+++ client/character/cha_man.gd
@@ -2,3 +2,6 @@ extends Node
 
 func _ready():
 	self.get_node("AnimationPlayer").play("idle")
+	
+func mirror(value):
+	get_node("AnimatedSprite3D").set_flip_h(value)
--- client/main/cha_move.gd
+++ client/main/cha_move.gd
@@ -1,24 +1,41 @@
 extends Spatial
 
 var speed = 3
+var current_animation = "idle"
+var old_animation = ""
 
 func _ready():
 	set_process(true)
 	
 func _process(delta):
+	#update the animation
+	if current_animation!=old_animation:
+		get_node("cha_man/AnimationPlayer").play(current_animation)
+		old_animation = current_animation
+	
+	# update the pos
 	var dir = Vector3(0,0,0)
 
-	if(Input.is_key_pressed(KEY_LEFT)):	
+	if(Input.is_action_pressed("cha_move_left")):	
 		dir.x -= 1
-	if(Input.is_key_pressed(KEY_RIGHT)):	
+	if(Input.is_action_pressed("cha_move_right")):	
 		dir.x += 1
-	if(Input.is_key_pressed(KEY_UP)):	
+	if(Input.is_action_pressed("cha_move_up")):	
 		dir.z -= 1
-	if(Input.is_key_pressed(KEY_DOWN)):	
+	if(Input.is_action_pressed("cha_move_down")):	
 		dir.z += 1
 	
 	if(dir.length_squared()>0):
+		current_animation ="run"
 		dir = dir.normalized()
 		var curPos = self.get_translation() + dir * speed * delta;
 		self.set_translation(curPos)
+		
+		# mirror
+		if(dir.x > 0):
+			get_node("cha_man").mirror(false)
+		elif(dir.x < 0):
+			get_node("cha_man").mirror(true)
+	else :
+		current_animation = "idle"
 		
