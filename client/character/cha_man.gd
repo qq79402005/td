fe7017a17787bc0310cde27a0d5a7c8d11153732
Albert Cameron <qq79402005@gmail.com>
2017-07-07 18:33:47 +0800

    59 脚步声

--- client/character/cha_man.gd
+++ client/character/cha_man.gd
@@ -8,14 +8,18 @@ func mirror(value):
 
 func fire_ball():
 	var bullet = preload("res://character/shoot/shoot.tscn").instance()
-	bullet.advance_speed = 10
+	bullet.advance_speed = 6
 	
 	if(get_node("AnimatedSprite3D").is_flipped_h()):
 		bullet.set_translation(Vector3(-0.5,0.5,0))
 	else:
 		bullet.set_translation(Vector3(1.5,0.5,0))
 	get_parent().add_child(bullet)
+	
+	# sound
+	#get_node("SamplePlayer2D").play("fire_sound")
 
 # 脚步声
 func foot_step(idx):
-	get_node("SamplerPlayer2D").play("foot_step");
+	get_node("SamplePlayer2D").play("foot_step_grass")
+	print("foot_step")
