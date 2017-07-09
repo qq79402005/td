1806a60e3e567350ca322d51d1ffacc8bd5ad214
qq79402005@gmail.com <qq79402005@gmail.com>
2017-07-09 19:43:23 +0800

    数据库链接成功。

--- server/src/db/db.java
+++ server/src/db/db.java
@@ -8,8 +8,8 @@ public class db {
 	public static void main(String args[]){
 		Connection c = null;
 		try{
-			//Class.forName("org.postgresql.Driver");
-			c = DriverManager.getConnection("jdbc.postgresql://localhost:5432/td", "postgres", "Q19870816q");
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/td", "postgres", "Q19870816q");
 		
 		
 			System.out.println("connected");
