8481510a249f5262915952056bbe939e8cb4c8d0
qq79402005@gmail.com <qq79402005@gmail.com>
2017-07-09 22:04:26 +0800

    协议生成。gdscript + java

---
+++ client/global/protocol/login.pb.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var account = int(0)
+var password = int(0)
+
+func _ready():
+	pass
---
+++ protocol/gen_protocol.py
@@ -0,0 +1,52 @@
+import os
+import shutil
+import json
+
+root_path = os.getcwd() + '/'
+java_save_path = root_path + "../server/src/protocol/"
+gd_save_path   = root_path + "../client/global/protocol/"
+
+# recreate java file save path
+if os.path.exists(java_save_path):
+    shutil.rmtree(java_save_path)
+
+os.mkdir(java_save_path)
+
+if os.path.exists(gd_save_path):
+    shutil.rmtree(gd_save_path)
+
+os.mkdir(gd_save_path)
+
+def gen_protocol_java( file):
+    protocol_name = os.path.splitext(file)[0]
+
+    java_file = open(java_save_path + protocol_name + ".pb.java", "w+")
+    java_file.writelines("package proto\n\n")
+    java_file.writelines("public class " + protocol_name + " {\n")
+    java_file.writelines("}\n")
+    java_file.close()
+
+def gen_protocol_godot(file):
+    protocol_name = os.path.splitext(file)[0]
+
+    gd_file = open(gd_save_path + protocol_name + ".pb.gd", "w+")
+    gd_file.writelines("extends Node\n\n")
+
+    with open(file) as data_file:
+        data = json.load(data_file) 
+        for key in data.keys():
+            gd_file.writelines("var " + key + " = int(0)\n")
+
+            print(key)
+        #print(data["account"])
+
+    gd_file.writelines("\n")
+    gd_file.writelines("func _ready():\n")
+    gd_file.writelines("\tpass\n")
+    gd_file.close()
+
+dirs = os.listdir(root_path)
+for file in dirs:
+    if os.path.splitext(file)[1] == '.proto':
+        gen_protocol_java( file)
+        gen_protocol_godot(file)
