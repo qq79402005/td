3072f2212751e937e2f4cc36d20bcec6eae43bd6
Albert Cameron <qq79402005@gmail.com>
2017-07-10 20:03:47 +0800

    ByteBuf 辅助类

--- build/godot/modules/network/bytebuf.cpp
+++ build/godot/modules/network/bytebuf.cpp
@@ -1,6 +1,6 @@
 #include "bytebuf.h"
 
-ByteBuf::ByteBuf() : Reference(), readIdx(NULL), writeIdx(0) {
+ByteBuf::ByteBuf() : Reference(), readIdx(NULL), writeIdx(0), data_size(0){
 
 }
 
@@ -9,13 +9,28 @@ ByteBuf::~ByteBuf() {
 
 void ByteBuf::writeInt32(int value)
 {
+	memcpy(&data[writeIdx], &value, 4);
+	writeIdx += 4;
+}
 
+void ByteBuf::resize(int size)
+{
+	data = new unsigned char[size];
+	data_size = size;
 }
 
 int ByteBuf::readInt32()
 {
     return 0;
 }
+
+// DVector<uint8_t> ByteBuf::rawArray()
+// {
+// 	DVector<uint8_t> result;
+// 
+// 	for( int i=0; i<data_size; i++)
+// 		result.push_back(data[i]);
+// }
 
 void ByteBuf::_bind_methods() 
 {
--- build/godot/modules/network/bytebuf.h
+++ build/godot/modules/network/bytebuf.h
@@ -12,14 +12,20 @@ public:
     ByteBuf();
     ~ByteBuf();
 
+	void resize(int size);
+
     void writeInt32(int value);
     int readInt32();
+
+	//DVector<uint8_t> rawArray();
 
 protected:
     static void _bind_methods();
 
     int     readIdx;
     int     writeIdx;
+	int			   data_size;
+	unsigned char* data;
 };
 
 #endif // OPENSIMPLEX_NOISE_H
